ecfb8e5c2dd43f54aaf71010eff10d12f13081a3
Pete Sellars <psellars@gmail.com>
2014-12-30 23:06:12 +1300

    Add initial fields to Pojo Add wiki links to README

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 Sandbox to test JUnit capabilities, third-party runners and interesting
 use cases.
 
+More information about the examples found here are available on the
+[JUnit Sandbox Wiki](https://github.com/petersellars/JUnit-Sandbox/wiki)
+
 ## Interesting Use Cases
 
 [OpenPojo](https://code.google.com/p/openpojo/) provides POJO (Plain Old
-Java Object) behaviour validation.
+Java Object) behaviour validation. [[Wiki Page]](https://github.com/petersellars/JUnit-Sandbox/wiki/OpenPojo)
--- src/main/java/net/catosplace/pojo/Customer.java
+++ src/main/java/net/catosplace/pojo/Customer.java
@@ -17,4 +17,45 @@ public final class Customer implements Serializable {
     public Customer() {
 
     }
+
+    /**
+     * Minimal business constructor
+     *
+     * @param firstName
+     */
+    public Customer(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Full Constructor
+     *
+     * @param id
+     * @param firstName
+     */
+    public Customer(final String id, final String firstName) {
+        this(firstName);
+        this.id = id;
+    }
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 }
